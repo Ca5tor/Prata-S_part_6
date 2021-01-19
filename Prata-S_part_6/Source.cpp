bd6b784963370801951f92e27f4f08484b7683b1
Ca5tor <48414173+Ca5tor@users.noreply.github.com>
2021-01-19 19:24:05 +0300

    Добавьте файлы проекта.

---
+++ Prata-S_part_6/Source.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+void Task_1() {
+	char ch;
+
+	cout << "Enter character (not ru), '@' character to complete " << endl;
+
+	while (cin >> ch, ch != '@'){					// Принимаем значения пока не будет введён символ '@'
+		if (isalpha(ch)) {							// Если символ буквенный
+			if (islower(ch))						// Если символ в нижнем регистре
+				cout << " -> " <<(char)toupper(ch);	// выводим его в верхнем регистре
+	
+			else if (isupper(ch))					// Если символ в верхнем регистре
+				cout << " -> " << (char)tolower(ch);// выводим его в нижнем регистре
+						
+		}
+		else cout << " -> " << ch << endl;			// Иначе выводим как есть
+
+		cout << endl;
+	}
+
+}
+
+int main() {
+	setlocale(0, "");
+
+	Task_1();
+
+	system("pause");
+	return 0;
+}
